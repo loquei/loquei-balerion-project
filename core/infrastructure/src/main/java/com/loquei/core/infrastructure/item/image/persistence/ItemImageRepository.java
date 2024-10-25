a603d20a82ce6ce5e5f450aab4950cca2fa39ade
Kayky Freitas <kayky_defreitas@hotmail.com>
2024-10-25 19:44:31 -0300

    fix(item): delete item images with item

--- core/infrastructure/src/main/java/com/loquei/core/infrastructure/item/image/persistence/ItemImageRepository.java
+++ core/infrastructure/src/main/java/com/loquei/core/infrastructure/item/image/persistence/ItemImageRepository.java
@@ -2,10 +2,14 @@ package com.loquei.core.infrastructure.item.image.persistence;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ItemImageRepository extends JpaRepository<ItemImageJpaEntity, String> {
 
     List<ItemImageJpaEntity> findAllByItemId(String itemId);
 
+    @Modifying
+    @Query("DELETE FROM ItemImageJpaEntity i WHERE i.itemId = :itemId")
     void deleteByItemId(String itemId);
 }
